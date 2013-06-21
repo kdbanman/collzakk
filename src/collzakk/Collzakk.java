35159cad47b808f04817a1a809f9d0e3ee1e9ec6
kdbanman <user@example.com>
2013-06-21 11:18:35 -0600

    FIRST

---
+++ src/collzakk/Collzakk.java
@@ -0,0 +1,162 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package collzakk;
+
+import java.util.ArrayList;
+import processing.core.*;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Collzakk extends PApplet {
+    
+    int windowSize;
+    
+    HashMap<Integer, Integer> nextTree;
+    
+    int seed;
+    int current;
+    
+    int max;
+    float radMult;
+    float angleStep;
+    
+    int minColor;
+    int maxColor;
+    
+    @Override
+    public void setup() {
+        windowSize = 900;
+        size(windowSize, windowSize);
+        
+        background(0xFF111111);
+        noStroke();
+        
+        nextTree = new HashMap<>();
+        
+        nextTree.put(2, 1);
+        seed = 2;
+        current = seed;
+        
+        max = 2147483646 / 2;
+        radMult = ((float) windowSize / 2) / log(max);
+        angleStep = 1.0f / log(max);
+        
+        minColor = color(71, 12, 0);
+        maxColor = color(252, 168, 151);
+    }
+    
+    @Override
+    public void draw() {
+        
+        if (nextTree.containsKey(current)) {
+            incrementSeed();
+            fill(magnitudeColor(seed));
+            current = seed;
+            println(seed);
+        } else{
+            ulamPlot(current);
+            int next = nextCollatz(current);
+            nextTree.put(current, next);
+            current = next;
+        }
+        
+        /* collision testing
+        
+        int colol = 0;
+        if (current % 3 == 0) colol = 0xFF0000FF;
+        else if (current % 2 == 0) colol = 0xFFFF0000;
+        else colol = 0xFF00FF00;
+        fill(colol);
+        ulamPlot(current);
+        current += 1;
+        * */
+    }
+
+    public static void main(String[] args) {
+        PApplet.main(new String[]{collzakk.Collzakk.class.getName()});
+    }
+    
+    public int nextCollatz(int n) {
+        if (n % 2 == 0) {
+            return n / 2;
+        } else {
+            return n * 3 + 1;
+        }
+    }
+    
+    public void incrementSeed() {
+        while (nextTree.containsKey(seed)) {
+            seed++;
+        }
+    }
+    
+    public int magnitudeColor(int x) {
+        float frac = 100 * sqrt((float) x / (float) max);
+        println(frac);
+        return lerpColor(minColor, maxColor, frac);
+    }
+    
+    public int factorColor(int x) {
+        return lerpColor(minColor, maxColor, (float) numPrimeFactors(x) / 31.0f );
+    }
+    
+    public int numPrimeFactors(int x) {
+        List<Integer> factors = new ArrayList<>();
+        for (int i = 2; i <= x; i++) {
+          while (x % i == 0) {
+            factors.add(i);
+            x /= i;
+          }
+        }
+        return factors.size();
+    }
+    
+    public void radPlot(int x) {
+        float r = radMult * log(x);
+        float theta = x * angleStep;
+        rect(windowSize/2 + (int) (r * cos(theta)), windowSize/2 + (int) (r * sin(theta)), 2, 2);
+    }
+    
+    public void ulamPlot(int n) {
+        /*
+         113379
+         1.0275954
+         113382
+         1.0275998
+         113383
+         Exception in thread "Animation Thread" java.lang.ArithmeticException: / by zero
+         at collzakk.Collzakk.ulamPlot(Collzakk.java:127)
+         at collzakk.Collzakk.draw(Collzakk.java:60)
+         at processing.core.PApplet.handleDraw(PApplet.java:2266)
+         at processing.core.PGraphicsJava2D.requestDraw(PGraphicsJava2D.java:243)
+         at processing.core.PApplet.run(PApplet.java:2140)
+         at java.lang.Thread.run(Thread.java:722)
+         */
+        int root = ceil(sqrt(n));
+        int shell = root + (root % 2 == 0 ? 1 : 0);
+        int dist = shell * shell - n;
+        int corner = dist / (shell - 1);
+        
+        int half = shell / 2;
+        dist = dist % (shell - 1);
+        int x, y;
+        if (corner == 0) {
+            x = -half;
+            y = half - dist;
+        } else if (corner == 1) {
+            x = -half + dist;
+            y = -half;
+        } else if (corner == 2) {
+            x = half;
+            y = -half + dist;
+        } else {
+            x = half - dist;
+            y = half;
+        }
+        
+        rect(windowSize / 2 + x, windowSize / 2 + y, 1, 1);
+    }
+}
