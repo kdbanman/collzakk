c3d35896ecaa88c0b3dbae4b2f7822b7d501f357
kdbanman <kirby.banman@gmail.com>
2013-12-30 07:03:21 -0700

    test script and restructure files slightly.

--- tovid.sh
+++ tovid.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-screens/ffmpeg -r 30 -i frame%06d.png -pix_fmt yuv420p -vcodec libx264 -crf 15 -s 900x900 -b:v 8000k out.mp4
+./ffmpeg -r 30 -i screens/frame%06d.png -pix_fmt yuv420p -vcodec libx264 -crf 15 -s 900x900 -b:v 8000k out.mp4
